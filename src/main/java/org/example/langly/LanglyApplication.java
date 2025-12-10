1c6b13685d10861b25f1d505fd6cd6a49113e613
anouar eil barry <elbarryanwar37@gmail.com>
2025-12-10 19:24:27 +0100

    first commit

---
+++ src/main/java/org/example/langly/LanglyApplication.java
@@ -0,0 +1,13 @@
+package org.example.langly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanglyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanglyApplication.class, args);
+    }
+
+}
