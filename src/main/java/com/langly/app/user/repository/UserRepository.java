a0b77170d4c96f789bdeba3f4a3a7fea89a7afc0
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-05 15:27:46 +0100

    add method to UserRepository for retrieving users by role

--- src/main/java/com/langly/app/user/repository/UserRepository.java
+++ src/main/java/com/langly/app/user/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.langly.app.user.repository;
 
+import com.langly.app.user.entity.Role;
 import com.langly.app.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
+    List<User> findAllByRole(Role role);
 }
