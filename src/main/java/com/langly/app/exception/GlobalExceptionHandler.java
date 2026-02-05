055a84094dcb4d2ca0908b79f941106bbd13e616
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-05 15:28:41 +0100

    add GlobalExceptionHandler to handle RoleNotFoundException

--- src/main/java/com/langly/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/langly/app/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.langly.app.exception;
 
+import com.langly.app.role.exception.RoleNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -34,11 +35,22 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex){
 
         ErrorResponse error = ErrorResponse.builder()
-                .status(HttpStatus.UNAUTHORIZED.value())
+                .status(HttpStatus.NOT_FOUND.value())
                 .message(ex.getMessage())
                 .build();
 
-        return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleRoleNotFoundException(RoleNotFoundException ex){
+
+        ErrorResponse error = ErrorResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .build();
+
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
 }
