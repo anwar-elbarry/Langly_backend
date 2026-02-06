d5dbeeb79c665f9531a304180e548bd57834efc8
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-06 10:24:04 +0100

    refactor RoleMapper to update package structure and integrate PermissionMapper

--- src/main/java/com/langly/app/Authority/web/mapper/RoleMapper.java
+++ src/main/java/com/langly/app/Authority/web/mapper/RoleMapper.java
@@ -1,14 +1,14 @@
-package com.langly.app.role.web.mapper;
+package com.langly.app.Authority.web.mapper;
 
-import com.langly.app.user.entity.Role;
-import com.langly.app.role.web.dto.request.RoleRequest;
-import com.langly.app.role.web.dto.response.RoleResponse;
+import com.langly.app.Authority.entity.Role;
+import com.langly.app.Authority.web.dto.request.RoleRequest;
+import com.langly.app.Authority.web.dto.response.RoleResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,uses = PermissionMapper.class)
 public interface RoleMapper {
 
     @Mapping(target = "id", ignore = true)
