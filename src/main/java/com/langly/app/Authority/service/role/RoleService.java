f3e745591ab3b7a15913330e9037ec537cc3583f
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-06 10:23:58 +0100

    refactor RoleService and RoleServiceImpl to update package structure and add permission assignment methods

---
+++ src/main/java/com/langly/app/Authority/service/role/RoleService.java
@@ -0,0 +1,17 @@
+package com.langly.app.Authority.service.role;
+
+import com.langly.app.Authority.web.dto.request.RoleRequest;
+import com.langly.app.Authority.web.dto.response.RoleResponse;
+
+import java.util.List;
+
+public interface RoleService {
+    RoleResponse create(RoleRequest request);
+    RoleResponse getById(String id);
+    RoleResponse getByName(String name);
+    List<RoleResponse> getAll();
+    RoleResponse update(String id, RoleRequest request);
+    void delete(String id);
+    RoleResponse assignPermissions(String roleId,List<String> permissionId);
+    RoleResponse takePermission(String roleId,List<String> permissionId);
+}
