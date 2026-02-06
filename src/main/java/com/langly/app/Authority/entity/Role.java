e72181aaa914bc665caf52df8596cd50575c21f7
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-06 10:23:45 +0100

    refactor Role entity to include permissions and update package structure

---
+++ src/main/java/com/langly/app/Authority/entity/Role.java
@@ -0,0 +1,27 @@
+package com.langly.app.Authority.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    private String id;
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions = new HashSet<>();
+}
