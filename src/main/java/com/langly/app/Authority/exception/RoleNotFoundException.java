c138bb47fe5929d654ddc6dabb07a454457c547c
anouar eil barry <elbarryanwar37@gmail.com>
2026-02-06 10:22:38 +0100

    rename RoleNotFoundException package for better organization

--- src/main/java/com/langly/app/Authority/exception/RoleNotFoundException.java
+++ src/main/java/com/langly/app/Authority/exception/RoleNotFoundException.java
@@ -1,4 +1,4 @@
-package com.langly.app.role.exception;
+package com.langly.app.Authority.exception;
 
 public class RoleNotFoundException extends RuntimeException {
     public RoleNotFoundException(String message) {
