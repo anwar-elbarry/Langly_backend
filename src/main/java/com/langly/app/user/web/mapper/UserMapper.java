113c9bd884ffec5c2e66b2690a42e63cdf5c2f39
anouar eil barry <elbarryanwar37@gmail.com>
2026-03-05 23:39:08 +0000

    add RoleMapper usage in UserMapper for improved role mapping

--- src/main/java/com/langly/app/user/web/mapper/UserMapper.java
+++ src/main/java/com/langly/app/user/web/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.langly.app.user.web.mapper;
 
+import com.langly.app.Authority.web.mapper.RoleMapper;
 import com.langly.app.user.entity.User;
 import com.langly.app.user.web.dto.request.UserRequest;
 import com.langly.app.user.web.dto.request.UserUpdateRequest;
@@ -8,13 +9,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = RoleMapper.class)
 public interface UserMapper {
 
     @Mapping(target = "schoolId", source = "school.id")
     UserResponse toResponse(User user);
 
-    @Mapping(target = "role.name",source = "roleName")
+    @Mapping(target = "role.name", source = "roleName")
     User toEntity(UserRequest request);
 
     User toUpdateEntity(UserUpdateRequest request);
