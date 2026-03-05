351462c175363e33d9c2e0d13c1084ac4b62e41b
anouar eil barry <elbarryanwar37@gmail.com>
2026-03-05 23:38:55 +0000

    add permissions field to RoleResponse for enhanced role management

--- src/main/java/com/langly/app/Authority/web/dto/response/RoleResponse.java
+++ src/main/java/com/langly/app/Authority/web/dto/response/RoleResponse.java
@@ -4,9 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
+@Setter
 @Builder
 public class RoleResponse {
     private String id;
     private String name;
+    private Set<PermissionResponse> permissions;
 }
