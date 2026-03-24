3a834dee91ef76c83e212124526f64908b910fbf
anouar eil barry <elbarryanwar37@gmail.com>
2026-03-24 17:28:38 +0100

    add: enable scheduling in LanglyApplication for task scheduling support

--- src/main/java/com/langly/app/LanglyApplication.java
+++ src/main/java/com/langly/app/LanglyApplication.java
@@ -3,8 +3,10 @@ package com.langly.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = {"com.langly.app", "com.langly.security"})
 public class LanglyApplication {
 
